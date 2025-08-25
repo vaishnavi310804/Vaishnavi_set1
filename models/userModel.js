c415fafdbf8f9239ab2844ecb210f419097feb4d
vaishnavi310804 <vaishnavikumari3108@gmail.com>
2025-08-25 15:16:10 +0530

    changes made

---
+++ models/userModel.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose'
+
+const userSchema= new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    profileImage: 
+    { type: String 
+
+    },
+  isVerified: 
+  { type: Boolean, 
+    default: false 
+},
+  verificationCode: 
+  { type: String 
+
+  },
+})
+
+const User=mongoose.model("User",userSchema);
+export default User
